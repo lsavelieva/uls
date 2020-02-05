f872da100f2f5de0413c9be203ba77d5269ea907
Liudmyla Savelieva <lsavelieva@e2r4p9.unit.ua>
2020-02-05 19:01:03 +0200

    prom var

--- inc/uls.h
+++ inc/uls.h
@@ -1,6 +1,12 @@
 #ifndef ULS_H
 #define ULS_H
 
+#define LS "uls: "
+#define FLAGS "-@ACGRSTaehlrtu"
+#define NOFILE ": No such file or directory"
+#define USAGE1 "uls: illegal option -- "
+#define USAGE2 "\nusage: uls [-@ACGRSTaehlrtu] [file ...]\n"
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -16,5 +22,19 @@
 #include <time.h>
 #include <sys/ioctl.h>
 #include "../libmx/inc/libmx.h"
+
+//declaration for dir & file as arguments
+/* typedef struct s_file {
+    char *f_mode;//file mode
+    int n_link;//number of links
+    char o_name;//owner name
+    char g_name;//group name
+    int size;//number of bytes in the file
+    char *month;//abbreviated month
+    int day;//day-of-month file was last modified
+    int hour;//hour file last modified
+    int minute;//minute file last modified
+    char *name;//pathname
+} t_file; */
 
 #endif
